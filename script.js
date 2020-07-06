92ffcd36eb162085838072843045a03c4eabaf97
Terry C <terrycetoute@gmail.com>
2020-07-06 12:42:59 -0400

    wrapped button around div, capture search val

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
+  $("#search-btn").on("click", function() {
+    var searchValue = $("#search-query").val();
+    // clear input box
+    $("#search-query").val("");
 
+    console.log(searchValue);
+  });
 });
