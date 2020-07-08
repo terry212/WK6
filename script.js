d65a2468245c54455e2ceac0143d30ce631e5a21
Terry C <terrycetoute@gmail.com>
2020-07-08 13:59:29 -0400

    search history appended

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-$(document).ready(function() {
-  $("#search-btn").on("click", function() {
-    var searchValue = $("#search-query").val();
-    // clear input box
+$(document).ready(function () {
+  $("#search-btn").on("click", function () {
+    var search = $("#search-query").val();
+    // clear search box
     $("#search-query").val("");
 
-    console.log(searchValue);
+    makeRow(search);
   });
+
+  $("#list-tab").on("click", "a", function () {
+    console.log($(this).text())
+  });
+
+  function makeRow(search) {
+    var aTag = $("<a>").addClass("list-group-item list-group-item-action").attr({ "id": search, "role": "list", "aria-controls": search, "data-toggle": "list" }).text(search);
+    $("#list-tab").append(aTag);
+  }
+
 });
