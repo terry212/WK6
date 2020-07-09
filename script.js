b3b69c9e8badaf09d80322bc5ac499acc83ff8f3
Terry C <terrycetoute@gmail.com>
2020-07-09 12:37:33 -0400

    weather data display, ability to find the weather for a given search

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 
-  var apiKey = "3e86ecd706ea3fa8f9a5cdead4a1ffb2";
+  var key = "3e86ecd706ea3fa8f9a5cdead4a1ffb2";
 
   $("#search-btn").on("click", function () {
     var search = $("#search-query").val();
@@ -22,15 +22,24 @@ $(document).ready(function () {
   function findWeather(search) {
     $.ajax({
       type: "GET",
-      url: `http://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${apiKey}&units=imperial`,
+      url: `http://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${key}&units=imperial`,
       dataType: "JSON",
       success: function (weather) {
 
         makeHistory(search);
         // clear old data
         $("#city-header").empty();
+        $("#weather-icons").empty();
+        $("#weather-info").empty();
         // Add new search data
-        $("#city-header").text(`${weather.name} (${new Date().toLocaleDateString()})`);
+        $("#city-header").text(`${weather.name} ( ${new Date().toLocaleDateString()} )`);
+        var img = $("<img>").attr("src", `http://openweathermap.org/img/w/${weather.weather[0].icon}.png`);
+        var windSpeed = $("<p>").addClass("list").text(`Wind Speed: ${weather.wind.speed} MPH`);
+        var humidity = $("<p>").addClass("list").text(`Humidity: ${weather.main.humidity} %`);
+        var temp = $("<p>").addClass("list").text(`Temperature: ${weather.main.temp} °F`);
+
+        $("#weather-info").append(windSpeed, humidity, temp);
+        $("#weather-icons").append(img);
 
       }
     })
